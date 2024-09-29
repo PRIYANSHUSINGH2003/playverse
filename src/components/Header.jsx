9fa5c9c8013aad704b5f0fe1fe6b14bfa8c6c424
Priyanshu Singh Rajawat <102310770+PRIYANSHUSINGH2003@users.noreply.github.com>
2024-09-29 17:06:44 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import { FaSearch, FaMoon, FaSun } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 import './Header.css'; // Import your header-specific styles
 
 const Header = ({ toggleDarkMode, darkMode, onSearch, searchValue }) => {
+    
     return (
         <header className={`header ${darkMode ? "dark-mode" : ""}`}>
             <div className="logo">
                 <h1>Games Playground</h1>
             </div>
+            <nav className="nav-links">
+                <Link to="/">Home</Link>
+                <Link to="/about">About Us</Link>
+                <Link to="/pc-games">PC Games</Link>
+                <Link to="/contact">Contact</Link>
+            </nav>
             <div className="search-bar">
                 <input 
                     type="text" 
