b049619379def9510fc9339380e752132cf03e2f
Priyanshu Singh Rajawat <102310770+PRIYANSHUSINGH2003@users.noreply.github.com>
2024-09-29 17:09:15 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
 import GamesPlayground from './pages/GamesPlayground';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer'
+import PCGames from './components/PCGames'; // Import the PCGames component
+import AboutUs from './components/AboutUs'; // Import the AboutUs component
+import Contact from './components/Contact'; // Import the Contact component
 import './App.css'; // Your provided CSS styles
 
 function App() {
@@ -11,10 +15,18 @@ function App() {
   };
 
   return (
-    <div className={`playground-container ${darkMode ? 'dark-mode' : ''}`}>
-      <GamesPlayground />
-      <Footer />
-    </div>
+    <Router>
+      <div className={`playground-container ${darkMode ? 'dark-mode' : ''}`}>
+        <Routes>
+          <Route path="/" element={<GamesPlayground />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/pc-games" element={<PCGames />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
