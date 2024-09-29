d64c8d024a8ebf5c9f1d5449d8689615c3057062
Priyanshu Singh Rajawat <102310770+PRIYANSHUSINGH2003@users.noreply.github.com>
2024-09-29 13:07:52 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -24,10 +24,10 @@ const Footer = () => {
                 <div className="footer-section social">
                     <h4>Follow Us</h4>
                     <div className="social-icons">
-                        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer"><FaFacebookF /></a>
-                        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
-                        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
-                        <a href="https://www.youtube.com" target="_blank" rel="noopener noreferrer"><FaYoutube /></a>
+                        <a href="https://www.facebook.com/priyanshusingh.rajawat.37" target="_blank" rel="noopener noreferrer"><FaFacebookF /></a>
+                        <a href="https://www.linkedin.com/in/priyanshu-singh-0859211b6/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+                        <a href="https://www.instagram.com/___priyanshusinghrajawat___/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
+                        <a href="https://www.youtube.com/@technicalworld9464" target="_blank" rel="noopener noreferrer"><FaYoutube /></a>
                     </div>
                 </div>
             </div>
